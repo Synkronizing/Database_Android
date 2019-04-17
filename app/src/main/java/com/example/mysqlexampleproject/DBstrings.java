18b95ff7d972153eb416730f4e7672cc7e66cb74
Synkronizing <josh6050@gmail.com>
2019-04-17 12:24:21 -0600

    Unfinished First upload

---
+++ app/src/main/java/com/example/mysqlexampleproject/DBstrings.java
@@ -0,0 +1,12 @@
+package com.example.mysqlexampleproject;
+
+public class DBstrings {
+    static final String DATABASE_URL = "192.168.1.107";
+    static final String DATABASE_NAME = "fruit";
+    static final String USERNAME = "root";
+    static final String PASSWORD = "toor";
+    //static final String DATABASE_URL = "gator4268.hostgator.com";
+    //static final String DATABASE_NAME = "mmcclend_fruit";
+    //static final String USERNAME = "mmcclend_fruity";
+    //static final String PASSWORD = "fruittoor";
+}
